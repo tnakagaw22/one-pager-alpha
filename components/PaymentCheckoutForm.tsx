b786c563626243b9d8538f89024c9cb5da7aa971
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-11-14 16:00:40 -0500

    use context to update payment error/success state

--- components/PaymentCheckoutForm.tsx
+++ components/PaymentCheckoutForm.tsx
@@ -1,50 +1,53 @@
 import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { CardElement, Elements, ElementsConsumer, useStripe, useElements } from "@stripe/react-stripe-js";
+import { Button } from "@chakra-ui/core";
 
-class CheckoutForm extends React.Component<{elements, stripe},{}> {
-    handleSubmit = async (event) => {
-      // Block native form submission.
-      event.preventDefault();
-  
-      const {stripe, elements} = this.props;
-  
-      if (!stripe || !elements) {
-        // Stripe.js has not loaded yet. Make sure to disable
-        // form submission until Stripe.js has loaded.
-        return;
-      }
-  
-      // Get a reference to a mounted CardElement. Elements knows how
-      // to find your CardElement because there can only ever be one of
-      // each type of element.
-      const cardElement = elements.getElement(CardElement);
-  
-      const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
-      });
-  
-      if (error) {
-        console.log('[error]', error);
-      } else {
-        console.log('[PaymentMethod]', paymentMethod);
-      }
-    };
-  
-    render() {
-      const {stripe} = this.props;
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <CardElement />
-          <button type="submit" disabled={!stripe}>
-            Pay
-          </button>
-        </form>
-      );
+import { GlobalContext } from "../context/GlobalContext";
+
+const CheckoutForm =(elements, stripe)=>{
+  const { setPaymentError, setIsPaidSuccessfully } = React.useContext(GlobalContext);
+
+  const handleSubmit = async (elements) => {
+    if (!elements.stripe || !elements.elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.elements.getElement(CardElement);
+
+    const {error, paymentMethod} = await elements.stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log('[error]', error);
+      setPaymentError(error.message);
+    } else {
+      setIsPaidSuccessfully();
     }
   }
+  
+  return (
+        <div>
+          <CardElement />
 
+          <Button 
+            variantColor="green" 
+            isDisabled={!stripe}
+            onClick={() => handleSubmit(elements)}
+            >Pay
+          </Button>
+
+        </div>
+      );
+
+}
 
 const InjectedCheckoutForm = () => {
     return (
